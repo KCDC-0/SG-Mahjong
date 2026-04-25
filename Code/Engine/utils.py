bfa968038aec805a3a74d96682d44cc6a5c34c14
KCDC <kcdcunderscore0@gmail.com>
2026-04-25 23:18:27 +0800

    Started on tile encoding

---
+++ Code/Engine/utils.py
@@ -0,0 +1,13 @@
+## extra functions
+
+
+import numpy as np
+
+def tile_array(tile_list):
+    '''Converts a list of tiles into an array for tracking and sending
+    
+    Rows: 
+    
+    '''
+    array = np.zeros(34, dtype=int)
+    # for tile in tile_list:
