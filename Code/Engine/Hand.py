7506dc8fb44518112cb06b10303e5de81045283f
KCDC <kcdcunderscore0@gmail.com>
2026-04-28 11:55:36 +0800

    Added hand class
    Implemented class to keep track of a hand

--- Code/Engine/Hand.py
+++ Code/Engine/Hand.py
@@ -1,1 +1,54 @@
 # hand class
+from Tile import Tile, Special_tile
+
+class Hand:
+    def __init__(self):
+        self.tiles = []
+        self.melds = []
+        self.special = []
+
+    def __str__(self):
+        ret_1 = ", ".join(str(t) for t in self.tiles)
+        ret_2 = ", ".join(str(t) for t in self.melds)
+        ret_3 = ", ".join(str(t) for t in self.special)
+        return ret_1 + '\n' + ret_2 + '\n' + ret_3
+
+    def add_tiles(self, tile_list: list):
+        '''Adds a given list of tiles to the hand'''
+        for tile in tile_list:
+            if tile.suit == 'special':
+                self.special.append(tile)
+            else:
+                self.tiles.append(tile)
+
+    def add_tile(self, tile: Tile):
+        '''Adds a given tile to the hand'''
+        if tile.suit == 'special':
+            self.special.append(tile)
+        else:
+            self.tiles.append(tile)
+
+    def remove_tile(self, tile: Tile):
+        '''Removes a given tile from the hand'''
+        self.tiles.remove(tile)
+
+    def sort(self):
+        '''Sorts hidden tiles in the hand'''
+        self.tiles.sort(key=lambda t: (t.suit, t.rank))
+
+    def add_meld(self, tile_list: list):
+        '''Forms 1 meld of tiles given the tiles in the meld'''
+        for tile in tile_list:
+            assert tile in self.tiles
+            self.tiles.remove(tile)
+        self.melds.append(tile_list)
+
+    @property
+    def tile_hidden_count(self):
+        '''Returns a count of hidden tiles'''
+        return len(self.tiles)
+    
+    @property
+    def tile_full_count(self):
+        '''Returns a count of non-special tiles, adjusting for kong melds'''
+        return len(self.tiles) + len(self.melds) * 3
--- Code/Tests/engine_tests.py
+++ Code/Tests/engine_tests.py
@@ -6,6 +6,7 @@ import sys
 sys.path.append('../Engine')
 from Tile import Tile, Special_tile
 from Table import Table
+from Hand import Hand
 import utils
 
 def test_add_tile():
@@ -42,4 +43,39 @@ def test_array():
     table1.draw()
     table1.deal(5)
     return utils.tile_array(table1.tiles)
+
+def test_hand_setup():
+    '''test if a hand works as intended:
+    Deals initial 13 tiles, separates special tiles from other and keeps dealing until a complete hand is reached
+    Sorts the hidden tiles in the hand
+    Adds a tile to the hand and removes one to simulate dealing
+    '''
+    table1 = Table('1')
+    table1.setup()
+    table1.shuffle()
+    my_hand = Hand()
+    my_hand.add_tiles(table1.deal(13))
+    while my_hand.tile_hidden_count < 13:
+        my_hand.add_tile(table1.draw())
+    print(my_hand)
+
+    return utils.tile_array(table1.tiles)
+
+def test_hand_meld():
+    '''test if a hand keeps track of melds as intended'''
+    table1 = Table('1')
+    table1.setup()
+    table1.shuffle()
+    my_hand = Hand()
+    my_hand.add_tile(table1.draw())
+    tl = []
+    for i in range(1,5):
+        tl.append(Tile("bamboo", 3, 'suited'))
+    my_hand.add_tiles(tl)
+    my_hand.add_meld(tl)
+    print(my_hand)
+    print(my_hand.tile_hidden_count)
+    print(my_hand.tile_full_count)
+    my_hand.add_meld(tl)
+
 
